36a21b3f198a254223b09a4043641a8f5adfdec3
Spectre Chase Project <pacmangoproject@gmail.com>
2018-01-09 02:47:31 -0800

    added gameloop file

--- src/gameloop.go
+++ src/gameloop.go
@@ -9,24 +9,17 @@
 
 package main
 
-import (
-	"github.com/veandco/go-sdl2/sdl"
-	"time"
+import(
+    "github.com/veandco/go-sdl2/sdl"
 )
 
-func renderSplash(r *sdl.Renderer) error {
+//Main gameloop
+func gameloop(r *sdl.Renderer) {
+}
+
+//Delete this when done
+func gameloopDebug(r *sdl.Renderer) {
+	renderSplash(r)
 	r.Clear()
-
-	//Debug code:
-	r.SetDrawColor(255, 0, 255, 255)
-	r1 := &sdl.Rect{X: 100, Y: 100, W: 55, H: 55}
-	r.DrawRect(r1)
-	r.FillRect(r1)
-	r.SetDrawColor(0, 0, 0, 255)
-
 	r.Present()
-
-	time.Sleep(time.Millisecond * 3000)
-
-	return nil
 }
