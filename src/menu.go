b5f6f1a17b9cad78e06d7efb2c5f9f2e8b2a34f6
Spectre Chase Project <pacmangoproject@gmail.com>
2018-01-11 00:45:00 -0800

    added menu file

---
+++ src/menu.go
@@ -0,0 +1,10 @@
+/**********************************************
+ *  Copyright 2018 The Spectre Chase Project  *
+ **********************************************
+ *   This software is made freely available   *
+ *   under the MIT license. See LICENSE.txt   *
+ *     for the full terms of this license.    *
+ **********************************************
+ */
+
+ package main
