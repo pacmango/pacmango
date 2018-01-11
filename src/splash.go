965facfd2d9015b94e5d7e20c33d562eb90a8789
Spectre Chase Project <pacmangoproject@gmail.com>
2018-01-11 00:57:21 -0800

    added splash red outline

--- src/splash.go
+++ src/splash.go
@@ -20,39 +20,46 @@ import (
 
 var titleTextLines = []string{"Spectre", "Chase"}
 var titleTextColor = sdl.Color{R: 250, G: 206, B: 52, A: 255}
+var titleTextOutlineColor = sdl.Color{R: 175, G: 0, B: 0, A: 255}
 var titleTextVerticalPos = []int32{100, 200, 300}
 
 //Create and display splash screen for three seconds
 func renderSplash(r *sdl.Renderer) error {
 	//Load font
-	titleFont, err := ttf.OpenFont(TitleFontAddress, 140)
+	titleFont, err := ttf.OpenFont(TitleFontAddress, 200)
 	if err != nil {
 		return fmt.Errorf("could not open title font")
 	}
 	defer titleFont.Close()
 
-	//Confiure font
-	titleFont.SetOutline(1)
+
+	r.Clear()
 
 	//Create title text surfaces
-	titleTextSurfaces := make([]*sdl.Surface, len(titleTextLines))
-
+	titleTextSurfaces := make([]*sdl.Surface, 2*len(titleTextLines))
 	for index, text := range titleTextLines {
-		titleTextSurfaces[index], err = titleFont.RenderUTF8Solid(text, titleTextColor)
+		titleTextSurfaces[index+2], err = titleFont.RenderUTF8Solid(text, titleTextColor)
+		if err != nil {
+			return fmt.Errorf("could not render title text surface for line %d", index+1)
+		}
+	}
+	titleFont.SetOutline(3)
+	for index, text := range titleTextLines {
+		titleTextSurfaces[index], err = titleFont.RenderUTF8Solid(text, titleTextOutlineColor)
 		if err != nil {
 			return fmt.Errorf("could not render title text surface for line %d", index+1)
 		}
 	}
 
 	//Create title text textures and render
-	r.Clear()
+
 
 	for index, surface := range titleTextSurfaces {
 		texture, err := r.CreateTextureFromSurface(surface)
 		if err != nil {
 			return fmt.Errorf("could not create title text texture from surface %d", index+1)
 		}
-		r.Copy(texture, nil, &sdl.Rect{X: 50, Y: titleTextVerticalPos[index], W: Width - 100, H: 100})
+		r.Copy(texture, nil, &sdl.Rect{X: 50, Y: titleTextVerticalPos[index%2], W: Width - 100, H: 100})
 	}
 
 	r.Present()
