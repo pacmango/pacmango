f7450ca53bfc0af6bc00ef354ece086eec11ab89
Spectre Chase Project <pacmangoproject@gmail.com>
2018-01-11 20:32:52 -0800

    Font struct/methods to new file

---
+++ src/textdraw/font.go
@@ -0,0 +1,38 @@
+/**********************************************
+ *  Copyright 2018 The Spectre Chase Project  *
+ **********************************************
+ *   This software is made freely available   *
+ *   under the MIT license. See LICENSE.txt   *
+ *     for the full terms of this license.    *
+ **********************************************
+ */
+
+ package textdraw
+
+ import(
+     "github.com/veandco/go-sdl2/ttf"
+     "image/color"
+ )
+
+ type Font struct {
+     Font *ttf.Font
+     Color color.RGBA
+ }
+
+ func NewTTFFont(fontFile string, fontSize int, color color.RGBA) (*Font, error) {
+     f, err := ttf.OpenFont(fontFile, fontSize)
+     return &Font{Font : f, Color : color}, err
+ }
+
+ func (f *Font) CloseTTFFont() {
+     f.Font.Close()
+     *f = Font{}
+ }
+
+ func (f *Font) SetColor(color color.RGBA) {
+     f.Color = color
+ }
+
+ func (f *Font) GetColor() color.RGBA {
+     return f.Color
+ }
--- src/textdraw/textdraw.go
+++ src/textdraw/textdraw.go
@@ -11,35 +11,10 @@
 
  import(
      "github.com/veandco/go-sdl2/sdl"
-     "github.com/veandco/go-sdl2/ttf"
-     "image/color"
  )
-
-type Font struct {
-    Font *ttf.Font
-    Color color.RGBA
-}
 
 type Window struct {
      Renderer *sdl.Renderer
      Width int32
      Height int32
  }
-
-func NewTTFFont(fontFile string, fontSize int, color color.RGBA) (*Font, error) {
-    f, err := ttf.OpenFont(fontFile, fontSize)
-    return &Font{Font : f, Color : color}, err
-}
-
-func (f *Font) CloseTTFFont() {
-    f.Font.Close()
-    *f = Font{}
-}
-
-func (f *Font) SetColor(color color.RGBA) {
-    f.Color = color
-}
-
-func (f *Font) GetColor() color.RGBA {
-    return f.Color
-}
